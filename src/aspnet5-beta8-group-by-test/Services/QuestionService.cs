f0f63be3da41028d4cda237162947f297345c0f3
Brent Russell <brussell@allsystemsgo.net>
2015-10-30 12:37:39 -0700

    add test project solution

---
+++ src/aspnet5-beta8-group-by-test/Services/QuestionService.cs
@@ -0,0 +1,61 @@
+﻿using aspnet5_beta8_group_by_test.Data;
+using aspnet5_beta8_group_by_test.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet5_beta8_group_by_test.Services
+{
+    public class QuestionService
+    {
+		private readonly ApplicationDbContext _dbContext;
+
+		public QuestionService(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		protected ApplicationDbContext DbContext
+		{
+			get
+			{
+				return _dbContext;
+			}
+		}
+
+		public class GroupByResult
+		{
+			public int Key { get; set; }
+
+			public DateTime MaxDate { get; set; }
+		}
+
+		public ExamInstanceQuestion[] BaselineQuery()
+		{
+			var query = from instance in this.DbContext.ExamInstances
+						join question in this.DbContext.ExamInstanceQuestions
+							on instance.Id equals question.ExamInstanceId
+						where instance.Id == 3
+						select question;
+
+			return query.ToArray();
+		}
+
+		public GroupByResult[] Query1()
+		{
+			var query = from instance in this.DbContext.ExamInstances
+						join question in this.DbContext.ExamInstanceQuestions
+							on instance.Id equals question.ExamInstanceId
+						where instance.Id == 3
+						group question by question.QuestionId into gQuestions
+						select new GroupByResult()
+						{
+							Key = gQuestions.Key,
+							MaxDate = gQuestions.Max(q => q.UtcDateUpdated)
+						};
+
+			return query.ToArray();
+		}
+	}
+}
---
+++ test/group-by-tests/QuestionServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xunit;
+using System.Threading;
+using System.Diagnostics;
+using aspnet5_beta8_group_by_test.Services;
+using aspnet5_beta8_group_by_test.Domain;
+
+namespace group_by_tests
+{
+    public class QuestionServiceTests : _DbContextServiceTestBase
+    {
+		private readonly QuestionService _questionService;
+		private DateTime _statsThresholdDate;
+
+        public QuestionServiceTests()
+			: base((services) =>services.AddScoped<QuestionService>())
+        {
+			_questionService = this.ServiceProvider.GetRequiredService<QuestionService>();
+
+			_statsThresholdDate = DateTime.UtcNow.AddDays(-10);
+			this.Setup_ExamInstance_Full_Stack_Test_Data(_statsThresholdDate);
+		}
+
+		#region Asserts
+
+		private void Assert_ExistsInCollection(IEnumerable<int> expected, IEnumerable<int> actual, string errorMessage)
+		{
+			Assert.All(actual, k => Assert.True(expected.Contains(k), errorMessage));
+		}
+
+		private void Assert_QuestionsAreNotInDiagnosticOrTopiclessRange(IEnumerable<Question> collection)
+		{
+			Assert.All(collection, q => Assert.True(q.Id > 40, "Diagnostic or topic-less question included."));
+		}
+
+		private void Assert_QuestionsAreLatestVersions(IEnumerable<Question> actualQuestions)
+		{
+			IEnumerable<Question> allQuestions = this.DbContext.Questions.AsEnumerable();
+
+			var query = from qActual in actualQuestions
+						join q in allQuestions
+							on qActual.RootQuestionId equals q.RootQuestionId
+						where q.Version > qActual.Version
+						select qActual;
+
+			Assert.Empty(query.ToArray());
+		}
+
+		private void Assert_QuestionsAreInUnansweredRange(IEnumerable<Question> actualQuestions)
+		{
+			// questions should be unanswered
+			Assert.All(actualQuestions, q => Assert.True((q.Id > 265 && q.Id < 511) || (q.Id > 755 & q.Id < 1001), "Question has already been answered"));
+		}
+
+		#endregion
+
+		[Fact]
+		public void BaselineQuery_Should_Not_Throw()
+		{
+			_questionService.BaselineQuery();
+		}
+
+		[Fact]
+		public void Query_Should_Not_Throw()
+		{
+			_questionService.Query1();
+		}
+	}
+}
